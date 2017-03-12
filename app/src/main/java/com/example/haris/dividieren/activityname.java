8d8eaf5a47b107877a7529be8f9b1e72382eb60b
pupi130396 <pupi130396@gmail.com>
2017-03-12 18:38:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/haris/dividieren/activityname.java
@@ -0,0 +1,15 @@
+package com.example.haris.dividieren;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Haris on 10.03.2017.
+ */
+
+public class activityname extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
